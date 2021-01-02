4dbbd72932c22dc4a202fffc1be073ffcd76f944
Sehmimul <59942853+Sehmimul@users.noreply.github.com>
2021-01-02 01:23:38 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -9,6 +9,7 @@ data = pd.read_csv('datafile.csv')
 # The data file was created by varying 'a' in mle.py until the value of lambda was 2.714.
 # A virtual machine from Amazon Web Services was used to find the values of 'a'. 
 # The values of 'a' were recorded for different values of energy.
+# Note that the values of 'a' are recorded in the y-axis (labelled as sigma_v).
 
 y = np.array(data['sigmav'], dtype=float)
 x = np.array(data['energy'], dtype=int)
